2d838c5daa102c235a1e69dcbc4d4f3e7cd0ca8b
Vitalii <vitalick066@gmail.com>
2022-06-19 11:39:45 +0300

    -Create HW-11 DOM

---
+++ DOM/HW-11 DOM_Intro/script.js
@@ -0,0 +1,8 @@
+function btnClick() {
+    const input = document.getElementById("text").value;
+    console.log(input)
+    document.getElementById('span-block').innerText = `Hello ${input}`;
+    const click = document.getElementById('main');
+    click.classList.toggle("main-d-none")
+
+}
